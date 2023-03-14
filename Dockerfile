c29b3269d8d3f85fba440137ec195dadd00e96ca
root <root@AminePc.localdomain>
2023-03-14 17:14:00 +0100

    now this image work on wsl ubuntu

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ RUN apt-get update -y; \
     mkdir apps
 COPY . /apps
 WORKDIR /apps
-RUN chmod +x node-nvm-nest-setup.sh && sh ./node-nvm-nest-setup.sh
-
+#RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+RUN chmod 777 node-nvm-nest-setup.sh && sed -i -e 's/\r$//' node-nvm-nest-setup.sh && ./node-nvm-nest-setup.sh
+#RUN nvm install node -y
+#RUN npm i -g @nestjs/cli
 
 #insatll npm and node and nestjs cli
 CMD "/bin/bash"
--- node-nvm-nest-setup.sh
+++ node-nvm-nest-setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install node -y
 npm i -g @nestjs/cli
